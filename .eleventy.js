c545d9736b53d8947dc0b18d8d55a0e761a294a5
Carsten Hoffmann <choffmann87@googlemail.com>
2022-10-18 00:14:39 +0000

    Initial version of website

--- .eleventy.js
+++ .eleventy.js
@@ -1,67 +1,65 @@
-const htmlmin = require("html-minifier");
-const markdownIt = require('markdown-it');
-const pluginRss = require("@11ty/eleventy-plugin-rss");
+const glob = require("glob-promise")
+const markdownIt = require("markdown-it")
+const fs = require("fs")
 
 module.exports = function (eleventyConfig) {
-  // PLUGINS
-  eleventyConfig.addPlugin(pluginRss);
+  function getFilename(path) {
+    let origFilename = path.split("/").pop()
+    let parts = origFilename.split(".")
+    parts.pop()
+    origFilename = parts.join(".")
+    return origFilename
+  }
 
-  // shortcode to render markdown from string => {{ STRING | markdown | safe }}
-  eleventyConfig.addFilter('markdown', function(value) {
-    let markdown = require('markdown-it')({
-      html: true
-    });
-    return markdown.render(value);
-  });
-
-  // rebuild on CSS changes
-  eleventyConfig.addWatchTarget('./src/_includes/css/');
+  const Image = require("@11ty/eleventy-img")
+  ;(async () => {
+    let options = {
+      widths: [128],
+      formats: ["png"],
+      outputDir: "public/128",
+      filenameFormat: function (id, src, width, format, options) {
+        const filename = getFilename(src)
+        return `${filename}.${format}`
+      },
+    }
+    let files = await glob("./src/raw/*.{jpg,jpeg,png,gif}")
+    const images = {}
+    for (const f of files) {
+      console.log("doing f", f)
+      const md = await Image(f, options)
+      const sign = getFilename(f)
+      const metadata = md.png.map((p) => {
+        return { format: p.format, height: p.height, width: p.width, url: `128/${sign}.png` }
+      })
+      images[sign] = metadata
+      console.log(metadata)
+    }
+    fs.writeFileSync(`public/index.json`, JSON.stringify(images, undefined, 2))
+  })()
 
   // Markdown
   eleventyConfig.setLibrary(
-    'md',
+    "md",
     markdownIt({
       html: true,
       breaks: true,
       linkify: true,
-      typographer: true
+      typographer: true,
     })
   )
 
-  //create collections
-  eleventyConfig.addCollection('sections', async (collection) => {
-    return collection.getFilteredByGlob('./src/sections/*.md');
-  });
-
   // STATIC FILES
-  eleventyConfig.addPassthroughCopy({ './src/static/': '/' });
-
-  // TRANSFORM -- Minify HTML Output
-  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-    if( outputPath && outputPath.endsWith(".html") ) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true
-      });
-      return minified;
-    }
-    return content;
-  });
+  eleventyConfig.addPassthroughCopy({ "./src/static/": "/" })
 
   return {
     dir: {
-      input: 'src',
-      output: 'public',
-      data: './_data',
-      includes: './_includes',
-      layouts: './_layouts'
+      input: "src",
+      output: "public",
+      data: "./_data",
+      includes: "./_includes",
+      layouts: "./_layouts",
     },
-    templateFormats: [
-      'md',
-      'njk',
-      '11ty.js'
-    ],
-    htmlTemplateEngine: 'njk'
-  };
-};
+    templateFormats: ["md", "njk", "11ty.js"],
+    htmlTemplateEngine: "njk",
+  }
+}
---
+++ src/index.md
@@ -0,0 +1,16 @@
+---
+layout: base.njk
+isFront: true
+---
+
+## Wilkommen bei Verkehrszeichen, simply served!
+
+Dieser webservice bietet alle Verkehrszeichen von [www.bast.de](https://www.bast.de/DE/Verkehrstechnik/Fachthemen/v1-verkehrszeichen/vz-download.html) einzeln addressierbar an, sodass du die Zeichen direkt auf einer anderen Webseite einbinden kannst.
+
+Probiere doch mal [/128/301.png](/128/301.png) aus. Aktuell werden alle Bilder in einer Breite von 128px als png ausgeliefert, weitere Formate sind in Zukunft denkbar. Mach ein Issue auf [GitHub](https://github.com/morl99/verkehrszeichen/issues) auf, wenn du Probleme oder Wünsche hast.
+
+Im [Index](index.json) sind alle verfügbaren Bilder aufgelistet.
+
+### Kontakt
+
+Ihr findet mich auf [Twitter](https://twitter.com/morl99).
